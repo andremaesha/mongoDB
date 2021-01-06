a5f8d312e3c0b4b71637d3ff880ec939866625a0
MMMM <61690805+andremaesha@users.noreply.github.com>
2021-01-06 22:47:10 +0700

    Add files via upload

---
+++ scripts/query-element.js
@@ -0,0 +1,33 @@
+// select * from products where category is null
+db.products.find({
+    category: {
+        $exists: false
+    }
+});
+
+
+// select * from products where type(category) = "string"
+db.products.find({
+    category: {
+        $type: "string"
+    }
+});
+
+db.products.find({
+    category: {
+        $type: "long"
+    }
+});
+
+// select * from products where type(price) in ("int", "long")
+db.products.find({
+    price: {
+        $type: ["int", "long"]
+    }
+});
+
+db.products.find({
+    price: {
+        $type: ["int", "long"]
+    }
+})
